47a356105ff5b807999128a29a0f19af68c3037c
pgpetrov2001 <pgpetrov2001@gmail.com>
2022-02-20 16:06:02 +0200

    Shortened rmq nlogn_1 implementation

--- RMQ/nlogn_1.hpp
+++ RMQ/nlogn_1.hpp
@@ -13,7 +13,6 @@ class RMQ {
         static const int MAXLOGN = log2(MAXN);
         int n;
         const T (&arr)[MAXN];
-        int jump_left[MAXN][MAXLOGN];
         int jump_right[MAXN][MAXLOGN];
         int log_table[MAXN];
         int pow_table[MAXLOGN];
@@ -36,20 +35,6 @@ class RMQ {
                 assert((1<<log) <= pow && pow < (1<<(log+1)));
             }
             int maxlog = log;
-            for (int i=0; i<n; ++i) {
-                jump_left[i][0] = i;
-                int j = i-1;
-                int log = 0;
-                while (j >= 0) {
-                    jump_left[i][log+1] = better(jump_left[j][log], jump_left[i][log]);
-                    log++;
-                    j += j-i;
-                }
-                while (log <= maxlog-1) {
-                    jump_left[i][log+1] = jump_left[i][log];
-                    log++;
-                }
-            }
             for (int i=n-1; i>=0; --i) {
                 jump_right[i][0] = i;
                 int j = i+1;
@@ -67,11 +52,11 @@ class RMQ {
         }
 
         int query(int l, int r) {
-            if (l > r) swap(l, r);
             if (l == r) return l;
+            if (l > r) swap(l, r);
             int amt = log_table[r-l];
             assert(2*(1<<amt) >= r-l+1);
-            return better(jump_right[l][amt], jump_left[r][amt]);
+            return better(jump_right[l][amt], jump_right[r-(1<<amt)+1][amt]);
         }
 };
 

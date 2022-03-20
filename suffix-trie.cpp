d2ba400b2d3a243f6e013d47211d4701cdbd0cd5
pgpetrov2001 <pgpetrov2001@gmail.com>
2022-03-20 22:05:22 +0200

    Fixed suffix trie algorithm

--- suffix-trie.cpp
+++ suffix-trie.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <vector>
 #include <cstring>
+#include <cassert>
 
 using namespace std;
 
@@ -54,6 +55,7 @@ int get_len(int q) {
         //contains data about the first state along the path from its parent to q
         //this state is for a string that is encountered once as a proper infix (if it's different from q)
         //and possibly once as a suffix, therefore pos is correct for q
+        //because once set, pos is not changed after that
         //q is a suffix
         return n - Q[q].pos;
     }
@@ -67,9 +69,7 @@ ImplicitState follow_word(int q, int k, int pos) { //assumes word is followable
         return {q};
     }
     char a = word[pos];
-    if (has_trans(q, a)) {
-        return {0};
-    }
+    assert(has_trans(q,a));
     int p = delta(q, a);
     int jump = get_len(p) - Q[q].len;
     if (k < jump) {
@@ -84,16 +84,16 @@ ImplicitState ImplicitState::follow_char(char b) const {
             return {0};
         }
         int p = delta(q, b);
-        if (Q[p].len - Q[q].len == 1) {
+        if (get_len(p) - Q[q].len == 1) {
             return {p};
         }
         return {q, 1, b};
     }
     int p = delta(q, a);
-    if (word[Q[p].pos + Q[q].len + k] != b) {
+    if (word[Q[p].pos + Q[q].len + k] != b) { 
         return {0};
     }
-    if (k == Q[p].len - Q[q].len - 1) {
+    if (k == get_len(p) - Q[q].len - 1) {
         return {p};
     }
     return {q, k+1, a};
@@ -124,30 +124,33 @@ string_view get_str(int q) {
 int qn = 1;
 ImplicitState stem;
 
-
-void add_char(char c) {
-    word[n++] = c;
+void add_char() {
+    char c = word[n++];
 
     int prev_stem = 0;
     while (stem && !stem.follow_char(c)) {
+        int inner_stem = 0;
+        //creation of positions is fucked up
         if (stem.k) { // make explicit state from implicit one
             int p = delta(stem.q, stem.a);
             Q[qn] = {Q[p].pos, Q[stem.q].len + stem.k}; // assign suffix link at next iteration
             delta(stem.q, stem.a) = qn;
             char a = word[Q[qn].pos + Q[qn].len];
             delta(qn, a) = p;
+            inner_stem = qn;
             if (prev_stem) {
-                Q[prev_stem].s_link = qn;
+                Q[prev_stem].s_link = inner_stem;
             }
-            stem = {qn};
-            qn++;
+            ++qn;
+        } else {
+            inner_stem = stem.q;
         }
         //create destination state
-        Q[qn] = {Q[stem.q].pos, Q[stem.q].len + 1};
-        delta(stem.q, c) = qn;
+        Q[qn] = {n - Q[inner_stem].len - 1, Q[inner_stem].len + 1};
+        delta(inner_stem, c) = qn;
         qn++;
 
-        prev_stem = stem.q;
+        prev_stem = inner_stem;
         stem = stem.get_s_link();
     }
 
@@ -156,7 +159,8 @@ void add_char(char c) {
     //therefore the current stem has that other transition as well apart from the transition with c
     //so the k of stem decreased during every iteration until it reached 0
 
-    if (prev_stem) {
+    if (prev_stem) { 
+        //stem is inner state due to above reasoning
         Q[prev_stem].s_link = stem.q; 
         // prev_stem might be newly created, otherwise its s_link is already the same, so OK
         // if stem is null then prev_stem is empty word, so OK
@@ -166,7 +170,7 @@ void add_char(char c) {
         stem = {1};
         //empty word is never a leaf
     } else {
-        stem = stem.follow_char(c); 
+        stem = stem.follow_char(c);
         // not a leaf because proper infix
     }
 }
@@ -175,7 +179,7 @@ void init() {
     qn = 1;
     n = 0;
     //create empty word
-    Q[qn] = {-1, 0}; // only it can be encountered before start of word
+    Q[qn] = {-1, 0}; 
     stem = {qn};
     qn++;
     Q[qn] = {0, 1};
@@ -186,8 +190,14 @@ void init() {
 int main() {
     cin >> word;
     int n = strlen(word);
-    init();
-    for (int i=0; i<n; ++i) {
-        add_char(word[i]);
+    init(); // init uses 1st letter
+    for (int i=1; i<n; ++i) {
+        add_char();
+    }
+    cout << qn-1 << " inner states: " << endl;
+    for (int q=1; q<=qn; ++q) {
+        cout << q << ' ';
+        cout << get_str(q) << endl;
+        cout << endl;
     }
 }
